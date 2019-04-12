3fa2e38be9b7f781f8325437a3ab1707d1ca907b
MohamedEsssam <me547010@gmail.com>
2019-04-12 20:09:58 +0200

    add send photo

--- server.cpp
+++ server.cpp
@@ -40,6 +40,34 @@ char** split(char* line)
     return tokens;
 }
 
+
+char** dotSplit(char* line)
+{
+    contentsize = 0;
+    //Array of Strings Where Each Position Has A Part of The Command
+    char **tokens = (char **)malloc(BUFFER_SIZE * sizeof(char *));
+    //A Pointer to Char To Hold Each Token
+    char *token;
+    //This Is Used To Eliminate New Line In The String
+    //Because It Causes Issues For strtok
+    char *newline = strchr(line,'\n');
+    if ( newline )
+        *newline = 0;
+
+    //strtok Is Used To Break Command Into a Series of Tokens Using
+    //Delimeter " "->Space
+    token = strtok(line, ".");
+    while (token != NULL)
+    {
+        tokens[contentsize++] = token;
+        token = strtok(NULL, ".");
+
+    }
+    tokens[contentsize] = NULL;
+    return tokens;
+}
+
+
 char* readFile(char* fileName)
 {
     FILE *file = fopen(fileName,"r");
@@ -70,9 +98,65 @@ void postFile(char ** requestContent)
     fclose(file);
 }
 
+int send_image(int socket){
+
+    FILE *picture;
+    int size, read_size, stat, packet_index;
+    char send_buffer[10240], read_buffer[256];
+    packet_index = 1;
+
+    picture = fopen("capture.jpeg", "r");
+    printf("Getting Picture Size\n");
+
+    if(picture == NULL) {
+        printf("Error Opening Image File"); }
+
+    fseek(picture, 0, SEEK_END);
+    size = ftell(picture);
+    fseek(picture, 0, SEEK_SET);
+    printf("Total Picture size: %i\n",size);
+
+    //Send Picture Size
+    printf("Sending Picture Size\n");
+    write(socket, (void *)&size, sizeof(int));
+
+    //Send Picture as Byte Array
+    printf("Sending Picture as Byte Array\n");
+
+    do { //Read while we get errors that are due to signals.
+        stat=read(socket, &read_buffer , 255);
+        printf("Bytes read: %i\n",stat);
+    } while (stat < 0);
+
+    printf("Received data in socket\n");
+    printf("Socket data: %c\n", read_buffer);
+
+    while(!feof(picture)) {
+
+        //Read from the file into our send buffer
+        read_size = fread(send_buffer, 1, sizeof(send_buffer)-1, picture);
+
+        //Send data through our socket
+        do{
+            stat = write(socket, send_buffer, read_size);
+        }while (stat < 0);
+
+        printf("Packet Number: %i\n",packet_index);
+        printf("Packet Size Sent: %i\n",read_size);
+        printf(" \n");
+        printf(" \n");
+
+        packet_index++;
+
+        //Zero out our send buffer
+        bzero(send_buffer, sizeof(send_buffer));
+    }
+}
+
 char *respondToRequest(char *request, int socket)
 {
     char ** requestContent = split(request);
+    char ** checkImageOrtext = dotSplit(request);
     char *fileContent = "" ;
     bool requestValid = true;
     if(strcmp(requestContent[0], "GET") == 0)
@@ -95,10 +179,13 @@ char *respondToRequest(char *request, int socket)
 
     if(requestValid)
     {
-        printf("%s", responseStatus);
-        strcat(responseStatus,fileContent);
-        send(socket, responseStatus, strlen(responseStatus), 0);
-
+        if (strcmp(checkImageOrtext[1],"txt")==0) {
+            printf("%s", responseStatus);
+            strcat(responseStatus, fileContent);
+            send(socket, responseStatus, strlen(responseStatus), 0);
+        } else{
+            send_image(socket);
+        }
     }
     else
     {
@@ -117,6 +204,7 @@ void *respond(void *new_socket)
     printf("%s\n",buffer);
     respondToRequest(buffer, newSocketFD);
 }
+
 
 int main()
 {
