fb25d31c9649f5d9b1a866254a428e098be8e62d
Mostafa Yousry <mostafayousry8@gmail.com>
2019-04-12 15:47:09 +0100

    make server always on and use threads to respond to client requests

--- server.cpp
+++ server.cpp
@@ -5,15 +5,18 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <iostream>
-
+#include <pthread.h>
 #define PORT 8080
 #define BUFFER_SIZE 1024
 using namespace std;
-char ok[1024] = "HTTP/1.0 200 OK\r\n";
-char badRequest[1024] = "HTTP/1.0 400 Bad Request\r\n";
+char buffer[1024] = {0};
+char responseStatus[1024];
+char badRequest[30] = "HTTP/1.0 400 Bad Request\r\n";
 int contentsize = 0;
 
-char** split(char* line){
+char** split(char* line)
+{
+    contentsize = 0;
     //Array of Strings Where Each Position Has A Part of The Command
     char **tokens = (char **)malloc(BUFFER_SIZE * sizeof(char *));
     //A Pointer to Char To Hold Each Token
@@ -37,12 +40,18 @@ char** split(char* line){
     return tokens;
 }
 
-char* readFile(char* fileName){
+char* readFile(char* fileName)
+{
     FILE *file = fopen(fileName,"r");
-    if (!file) {printf("file not found");}
+    if (!file)
+    {
+        printf("file not found\n");
+        return "";
+    }
     char *fileContent = (char *) malloc(sizeof(char) * 1024);
     char *lineContent = (char *) malloc(sizeof(char) * 1024);
-    while (!feof(file)) {
+    while (!feof(file))
+    {
         fgets(lineContent, sizeof(file),file);
         strcat(fileContent,lineContent);
         strcpy(lineContent,"\0");
@@ -51,38 +60,62 @@ char* readFile(char* fileName){
     return fileContent;
 }
 
-void postFile(char ** requestContent){
+void postFile(char ** requestContent)
+{
     FILE *file = fopen(requestContent[1],"a+");
-    for (int i = 2; i< contentsize; i++){
-        fprintf(file, "%s", requestContent[i]);
+    for (int i = 2; i< contentsize; i++)
+    {
+        fprintf(file, "%s ", requestContent[i]);
     }
     fclose(file);
 }
 
-void respondToRequest(char *request, int socket){
+char *respondToRequest(char *request, int socket)
+{
     char ** requestContent = split(request);
     char *fileContent = "" ;
-    bool responsStatus = true;
-    if(strcmp(requestContent[0], "GET") == 0){
+    bool requestValid = true;
+    if(strcmp(requestContent[0], "GET") == 0)
+    {
         fileContent = readFile(requestContent[1]);
+        if(strcmp(fileContent, "") == 0)
+            strcpy(responseStatus, "HTTP/1.0 404 Not Found\r\n");
+        else
+            strcpy(responseStatus, "HTTP/1.0 200 OK\r\n");
     }
-    else if(strcmp(requestContent[0], "POST") == 0){
+    else if(strcmp(requestContent[0], "POST") == 0)
+    {
         postFile(requestContent);
+        strcpy(responseStatus, "HTTP/1.0 200 OK\r\n");
     }
-    else{
-        responsStatus = false;
-    }
-
-    if(responsStatus){
-        strcat(ok,fileContent);
-        send(socket , ok , strlen(ok) , 0 );
-        printf("200 ok send\n");
-    }
-    else{
-        send(socket , badRequest , strlen(badRequest) , 0 );
-        printf("400 bad request send\n");
+    else
+    {
+        requestValid = false;
     }
 
+    if(requestValid)
+    {
+        printf("%s", responseStatus);
+        strcat(responseStatus,fileContent);
+        send(socket, responseStatus, strlen(responseStatus), 0);
+
+    }
+    else
+    {
+        send(socket, badRequest, strlen(badRequest), 0 );
+        printf("%s", badRequest);
+    }
+
+}
+
+void *respond(void *new_socket)
+{
+
+    int *temp = (int *) new_socket;
+    int newSocketFD = *temp;
+    read(newSocketFD, buffer, 1024);
+    printf("%s\n",buffer);
+    respondToRequest(buffer, newSocketFD);
 }
 
 int main()
@@ -91,9 +124,7 @@ int main()
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-
-    char buffer[1024] = {0};
-
+    pthread_t respondThread;
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
@@ -118,18 +149,23 @@ int main()
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
+
     if (listen(server_fd, 3) < 0)
     {
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+
+    while(true)
     {
-        perror("accept");
-        exit(EXIT_FAILURE);
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        pthread_create(&respondThread, NULL, respond, (void*) &new_socket);
     }
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    respondToRequest(buffer, new_socket);
+
     return 0;
 }
